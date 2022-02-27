7fcd4b4113f1025de7c1d081a94234b5e0ad1207
GailC7 <99687517+GailC7@users.noreply.github.com>
2022-02-27 12:44:29 -0500

    Update NumberPuzzle.java

--- NumberPuzzle.java
+++ NumberPuzzle.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class NumberPuzzle
 {
-    public static void main(String[] args)
+    public static void main(String[] args) //should not be main
     {
         Scanner kb = new Scanner(System.in);
         Random random = new Random();
