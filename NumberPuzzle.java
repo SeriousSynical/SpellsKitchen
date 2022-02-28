9d73fa79e002d1eb383d2eea18924a25a4df7456
GailC7 <99687517+GailC7@users.noreply.github.com>
2022-02-28 17:26:30 -0500

    Update NumberPuzzle.java

--- NumberPuzzle.java
+++ NumberPuzzle.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class NumberPuzzle extends 
+public class NumberPuzzle extends character
 {
     public static void NumberPuzzle //should not be main
     {
@@ -19,26 +19,27 @@ public class NumberPuzzle extends
         
 
         //System.out.println(x);
-        System.out.println("You see a cabinet with a crow a top, ''Guess the correct number to pass'' screeches the crow");
-        System.out.println("Time to spam numbers");
-        System.out.println("Try a number between 1-1000 for the code");
+        System.out.println("You see a cabinet with a crow a top);
+        System.out.println("Crow: Guess the correct number to pass" + name "!!");
+        System.out.println(name +":" + " Time to spam numbers");
+        System.out.println("Crow: Try a number between 1-1000 for the code");
         do
         {
-            System.out.println("Guess a number again between 1-1000");
+            System.out.println("Crow: Guess a number again between 1-1000");
             guessedNumber = kb.nextInt();
             if(guessedNumber < randomNumber)//guess is low
             {
-                message = guessedNumber + " is to low fool";
+                message = "Crow: " + guessedNumber + " is to low fool";
                 System.out.println(message);
             }
             else if(guessedNumber > randomNumber)//guess is high
             {
-                message = guessedNumber + " is to high fool";
+                message = "Crow: " + guessedNumber + " is to high fool";
                 System.out.println(message);
             }
             else//guess is correct
             {
-                message = guessedNumber + " is to correct";
+                message = "Crow: " + guessedNumber + " is to correct";
                 System.out.println(message);
                 correct = true;
 
