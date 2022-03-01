c5ca306693ed6b7d87e5ac6c721d1222c2ceb6f7
GailC7 <99687517+GailC7@users.noreply.github.com>
2022-02-28 22:19:32 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,128 @@
 import java.util.Scanner;
 
-public class Main {
+    public class Main 
+    {
+        public static void main(String[] args) 
+        {
+            int choice;
+            Scanner kb = new Scanner(System.in);
 
-    public static void main(String[] args) {
+            System.out.println("");
+            System.out.println("*Game greeting*");
 
-        run();
+            System.out.println("Select an action");
+            System.out.println("\t 1. Inspect room ");
+            System.out.println("\t 2. Talk to (NPC) ");
+            System.out.println("\t 3. View inventory");
+            System.out.println("\t 4. (other actions)");
+            choice = kb.nextInt();
+
+            switch(choice)
+            {
+                //Inspect room
+                case 1:
+                    int roomChoice;
+                    System.out.println("Select an area to inspect");
+                    System.out.println("\t 1. The kitchen");
+                    System.out.println("\t 2. The pantry");
+                    System.out.println("\t 3. The dining area");
+                    System.out.println("\t 4. Magic ingredient shelf");
+                    System.out.println("\t 0. Go back");
+
+                    roomChoice = kb.nextInt();
+
+
+
+                        switch(roomChoice)
+                            {
+                                //kitchen
+                                case 1:
+                                    System.out.println("*Explore kitchen* + actions");
+                                break;
+
+                                //pantry
+                                case 2:
+                                    System.out.println("*Explore pantry* + actions");
+                                break;
+
+                                //dining
+                                case 3:
+                                    System.out.println("*Explore dining room* + actions");
+                                break;
+
+                                //Magic shelf
+                                case 4:
+                                    System.out.println("*Explore Shelf* + actions");
+                                break;
+
+                                default:
+                                System.out.println("Invalid");
+                            }   
+                    //do{}while(choice != 0);
+
+                break;
+
+                //Talk to npc
+                case 2:
+                    System.out.println("*Talk to npc (gives clues)*");
+                break;
+
+                //View inventory
+                case 3:
+                    int itemChoice;
+
+                    System.out.println("print getInventory();");
+                    System.out.println("Select inventory action");
+                    System.out.println("\t 1. Inspect item");
+                    System.out.println("\t 2. Use");
+                    System.out.println("\t 3. Eat");
+                    System.out.println("\t 4. Other");
+                    System.out.println("\t 0. Go back");
+
+                    itemChoice = kb.nextInt();
+                        switch(itemChoice)
+                        {
+                            //Inspect
+                            case 1:
+                                System.out.println("*select item and give description*");
+                            break;
+
+                            //Use
+                            case 2:
+                                System.out.println("*Use item if condions are met use/fail to use*");
+
+                            break;
+
+                            //Eat
+                            case 3:
+                                System.out.println("*consuming certain foods do nothing, one will have a key piece embeded*");
+
+                            break;
+
+                            //Other
+                            case 4:
+                                System.out.println("Other actions");
+                            break;
+                        }
+
+                break;
+
+                //Other actions
+                case 4:
+                    System.out.println("");
+                break;
+
+                default:
+                System.out.println("Invalid");
+            }//end Choice actions
+        kb.close();
+        }
 
     }
+        //=========================================Action choices that link to other methods^^
+        run();
+
+  }//end Main
 
     public static void run() {
 
