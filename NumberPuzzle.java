a58f71550f5ef09dff0ccc3f59533baef4870ddd
GailC7 <99687517+GailC7@users.noreply.github.com>
2022-03-23 17:03:33 -0400

    crow
    crow

--- NumberPuzzle.java
+++ NumberPuzzle.java
@@ -40,6 +40,56 @@ public class NumberPuzzle //extends character
         
         //System.out.println(x);
         System.out.println("You see a cabinet with a crow a top");
+        System.out.println("      ████████ ");  
+        System.out.println("  ██████  ████   ");
+        System.out.println("██████████████  ");   
+        System.out.println("      ████████   ");  
+        System.out.println("      ████████████████ "); 
+        System.out.println("       "); 
+        System.out.println("        "); 
+        System.out.println("     "); 
+        System.out.println("     ");                                   
+        System.out.println("   "); 
+        System.out.println("    "); 
+        System.out.println("      "); 
+        System.out.println("                          "); 
+        System.out.println("          ");   
+        System.out.println("                "); 
+        System.out.println("           "); 
+        System.out.println("         ");                                        
+                          
+          ████████                                          
+        ██████  ████                                          
+      ██████████████                                          
+            ██████████                                        
+            ████████████████                                  
+            ██████████████████████                            
+            ████████████████████████████                      
+              ████████████████████████████                    
+              ██████████████████████████████████              
+              ████████████████████████████████████            
+                  ████████████████████████████████████████████
+                    ████████████████████████████████████████  
+                        ████████████████                      
+                          ██    ██                            
+                          ██    ██                            
+                          ██    ██                            
+                      ██████████████                          
+      
+                                                
+                                             
+                                      
+                                  
+                                 
+                          
+                      
+                  
+                    
+                        
+                                                     
+                                              
+                                               
+                                           
         System.out.println("Crow: Try a number between 1-1000 for the code");
 
         do
